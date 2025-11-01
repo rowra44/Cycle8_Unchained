b79003d5f2693367fbf3664cafeb42792e56828d
root <root@dockerhost.rowra.org>
2025-11-01 22:19:00 +0100

    added uf2boot in its current hacky state

---
+++ uf2boot/src/build/BLUEPILL/stm32f103/backup.d
@@ -0,0 +1,16 @@
+build/BLUEPILL/./stm32f103/backup.o: stm32f103/backup.c \
+ ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/rtc.h \
+ ../libopencm3/include/libopencm3/stm32/f1/rtc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/f1/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_all.h \
+ stm32f103/backup.h
---
+++ uf2boot/src/build/BLUEPILL/winusb.d
@@ -0,0 +1,7 @@
+build/BLUEPILL/winusb.o: winusb.c \
+ ../libopencm3/include/libopencm3/usb/usbd.h \
+ ../libopencm3/include/libopencm3/usb/usbstd.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdbool.h winusb.h \
+ winusb_defs.h usb_conf.h
